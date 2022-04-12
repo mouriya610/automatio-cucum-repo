02e231e98d2196d515713ce74f19c79cb3b529ff
mouriya610 <mouriyanethaji@gmail.com>
2022-04-12 15:19:36 +0530

    Feature file and Step definition done

---
+++ src/main/java/com/Mou_Proj/App.java
@@ -0,0 +1,13 @@
+package com.Mou_Proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
